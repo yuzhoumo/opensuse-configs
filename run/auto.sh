c05a321ceb95e3f8d306a31e821ba3f868e72b0f
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-12-19 14:54:04 -0800

    Add Gnome automations

--- run/auto.sh
+++ run/auto.sh
@@ -15,4 +15,8 @@ printf "\n\x1b[33m### Install packages\x1b[0m\n\n"
 printf "\n\x1b[33m### Install dotfiles\x1b[0m\n\n"
 ../scripts/dot.sh
 
+# Launch Gnome automation
+printf "\n\x1b[33m### Set Gnome configurations\x1b[0m\n\n"
+../scripts/gnome.sh
+
 printf "\n\x1b[32mDone with setup! Please reboot.\x1b[0m\n\n"
--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -3,5 +3,44 @@
 # Author: Yuzhou "Joe" Mo (@yuzhoumo)
 # License: GNU GPLv3
 
+# Set wallpaper
+printf "\nSetting wallpaper...\n"
+wallpaper_path="${HOME}/Pictures/wallpaper.png"
+cp ../assets/images/wallpaper.png "$wallpaper_path"
+gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper_path}"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${wallpaper_path}"
+
 # Add ibus input to Gnome kitty shortcut
+printf "\nSetting kitty shortcut to use ibus...\n"
 sudo cp ../assets/gnome/kitty.desktop /usr/share/applications/kitty.desktop
+
+# Set Gnome shell theme
+printf "\nSetting Gnome shell theme to catppuccin...\n"
+theme="catppuccin-mocha-lavender"
+mkdir -p "${HOME}/.themes" && \
+  cp -r "../assets/gnome/${theme}/." "${HOME}/.themes/${theme}/."
+gsettings set org.gnome.shell.extensions.user-theme name catppuccin-mocha-lavender
+gsettings set org.gnome.desktop.interface gtk-theme catppuccin-mocha-lavender
+
+# Reset app picker shortcuts to alphabetical order
+printf "\nSorting app picker shortcuts alphabetically...\n"
+gsettings set org.gnome.shell app-picker-layout "[]"
+
+# Show all max/min/close buttons in title bar
+printf "\nEnabling all max/min/close buttons in title bars...\n"
+gsettings set org.gnome.desktop.wm.preferences button-layout appmenu:minimize,maximize,close
+
+# Disable mouse acceleration
+printf "\nDisabling mouse acceleration...\n"
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
+
+# Install Gnome extensions
+#  307 | Dash to Dock
+#  744 | Hide Activities Button
+#    7 | Removable Drive Menu
+#  355 | Status Area Horizontal Spacing
+# 2890 | Tray Icons: Reloaded
+# 5470 | Weather O'Clock
+printf "\nInstalling Gnome extensions...\n\n"
+../assets/gnome/gnome-shell-extension-installer 307 744 7 355 2890 5470
+
